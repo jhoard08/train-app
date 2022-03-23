0b363c4325ebe48d52bd60d356034d28c8002037
Jake <jacob37857@gmail.com>
2022-03-22 21:55:26 -0400

    working fetch data

--- train-app/src/App.js
+++ train-app/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
+//https://api.wmata.com/TrainPositions/TrainPositions?contentType=json&api_key=
+
 function App() {
+
+  const getTrain = () => {
+    fetch('https://api.wmata.com/TrainPositions/TrainPositions?contentType=json&api_key=')
+      .then(response => response.json())
+      .then(data => console.log(data));
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getTrain}> Get a train!</button>
     </div>
   );
 }
