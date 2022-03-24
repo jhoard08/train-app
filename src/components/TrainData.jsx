1a77c408ab02ec776cfd72ba0ec1d4b37564d573
Jake <jacob37857@gmail.com>
2022-03-23 23:21:48 -0400

    display table data

--- src/components/TrainData.jsx
+++ src/components/TrainData.jsx
@@ -14,23 +14,39 @@ const TrainData = () => {
     const response = await fetch('https://api.wmata.com/TrainPositions/TrainPositions?contentType=json&api_key=207e280fe77d4a3aa17ebf7ee8ca9b14');
     const jsonData = await response.json();
     setTrainData(jsonData);
+
   };
 
   if (trainData === undefined) {
     return <div>Still Loading.....</div>
   }
 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Train Data</h2>
-      </header>
-      <div className="user-container">
-        <h5 className="info-item">Train Id: {trainData.TrainPositions[0].TrainId}</h5>
-        <h5 className="info-item">Car Count: {trainData.TrainPositions[0].TrainId}</h5>
-        <h5 className="info-item">Line Code: {trainData.TrainPositions[0].TrainId}</h5>
-        <h5 className="info-item">Train Number:{trainData.TrainPositions[0].TrainId}</h5>
-      </div>
+    <div className="titleHead">
+      <h1>Train Data</h1>
+      <table className="tableContainer">
+        <thead>
+          <tr>
+            <th>Train ID</th>
+            <th>Car Count</th>
+            <th>Train Number</th>
+            <th>Code Line</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            trainData.TrainPositions.map((train) => (
+              <tr key={train.TrainPositions}>
+                <td>{train.TrainId}</td>
+                <td>{train.CarCount}</td>
+                <td>{train.TrainNumber}</td>
+                <td>{train.LineCode}</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
     </div>
   )
 }
